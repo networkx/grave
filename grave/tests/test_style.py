ceb7120ac38b64e8663d3e50cd7d39f3d0a50454
Camille Scott <camille.scott.w@gmail.com>
2018-03-29 15:54:26 -0700

    Test edge style application

--- grave/tests/test_style.py
+++ grave/tests/test_style.py
@@ -34,3 +34,40 @@ def test_generate_node_style_typerror(barbell_network):
     with pytest.raises(TypeError):
         node_styles = generate_node_styles(barbell_network,
                                        'red')
+
+
+def test_generate_edge_styles_callable(barbell_network):
+    from grave.style import apply_style, generate_edge_styles
+    
+    def style_func(attrs):
+        return {'color': 'red'}
+
+    edge_styles = generate_edge_styles(barbell_network,
+                                       style_func)
+    
+    for edge, style in edge_styles.items():
+        assert edge in barbell_network.edges
+        assert style == {'color': 'red'}
+
+
+def test_generate_edge_styles_dict(barbell_network):
+    from grave.style import apply_style, generate_edge_styles
+
+    style = {'color': 'red'}
+
+    edge_styles = generate_edge_styles(barbell_network,
+                                       style)
+
+    for edge, style in edge_styles.items():
+        assert edge in barbell_network.edges
+        assert style == {'color': 'red'}
+
+
+def test_generate_edge_style_typerror(barbell_network):
+    from grave.style import apply_style, generate_edge_styles
+
+    with pytest.raises(TypeError):
+        edge_styles = generate_edge_styles(barbell_network,
+                                       'red')
+
+
