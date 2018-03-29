5792e43d64f0db519b81ab1db2f58469d7621b6b
Camille Scott <camille.scott.w@gmail.com>
2018-03-29 13:21:54 -0700

    Add tests submodule, fix up setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import setup
+from setuptools import find_packages
 
-setup(name="GraVE",
-      version="0.0.1",
-      description="Dead simple graph visualization",
-      long_description="GraVE is a Graph Visualization Package combining ideas from Matplotlib and NetworkX",
-      url="http://github.com/networkx/grave",
-      author="NetworkX Developers",
-      author_email="networkx-discuss@googlegroups.com",
-      license="BSD",
-      packages=["grave"]
-      tests_require=['pytest', 'pytest-runner'],
-      setup_requires['pytest-runner'],
-      zip_safe=False)
+setup(name = "GraVE",
+      version = "0.0.1",
+      description = "Dead simple graph visualization",
+      long_description = "GraVE is a Graph Visualization Package "\
+                         "combining ideas from Matplotlib and NetworkX",
+      url = "http://github.com/networkx/grave",
+      author = "NetworkX Developers",
+      author_email = "networkx-discuss@googlegroups.com",
+      license = "BSD",
+      packages = find_packages(),
+      tests_require = ['pytest', 'pytest-runner'],
+      setup_requires = ['pytest-runner'],
+      zip_safe = False)
