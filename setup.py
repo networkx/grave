dca5a85c2e41375134f096198130ff5e1b54cbfd
Paul Ivanov <pi@berkeley.edu>
2018-03-28 22:31:07 -0700

    it's an official package now!

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name="GraVE",
+      version="0.0.1",
+      description="Dead simple graph visualization",
+      long_description="GraVE is a Graph Visualization Package combining ideas from Matplotlib and NetworkX",
+      url="http://github.com/networkx/grave",
+      author="NetworkX Developers",
+      author_email="networkx-discuss@googlegroups.com",
+      license="BSD",
+      packages=["grave"],
+      zip_safe=False)
