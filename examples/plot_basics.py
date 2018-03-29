c8cab2dbb04abe494dce664584c0b8287f51ae29
Thomas A Caswell <tcaswell@gmail.com>
2018-03-29 16:08:34 -0700

    DOC: add shape and color to plot_basic

--- examples/plot_basics.py
+++ examples/plot_basics.py
@@ -6,21 +6,28 @@ Test
 """
 import networkx as nx
 import matplotlib.pyplot as plt
-
+import random
 from grave import grave
 
 
 graph = nx.barbell_graph(10, 14)
+
 nx.set_node_attributes(graph, dict(graph.degree()), 'degree')
 
 
 def degree_colorer(node_attributes):
     deg = node_attributes['degree']
+    shape = random.choice(['s', 'o', '^', 'v', '8'])
     if deg > 5:
-        return {'color': 'r', 'size': 10*deg}
-    return {'color': 'b', 'size': 10*deg}
+        return {'color': 'r', 'size': 20*deg, 'shape': shape}
+    return {'color': 'b', 'size': 20*deg, 'shape': shape}
+
+
+def pathological_edge_style(edge_attrs):
+    return {'color': random.choice(['r', (0, 1, 0, .5), 'xkcd:ocean'])}
 
 
 fig, ax = plt.subplots()
-grave.plot_network(graph, ax=ax, node_style=degree_colorer)
+grave.plot_network(graph, ax=ax, node_style=degree_colorer,
+                   edge_style=pathological_edge_style)
 plt.show()
