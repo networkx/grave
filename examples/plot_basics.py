e4280295f9f5ec69c204210d20452e755b29d602
Thomas A Caswell <tcaswell@gmail.com>
2018-03-29 14:25:25 -0700

    DOC: update example to color based on degree

--- examples/plot_basics.py
+++ examples/plot_basics.py
@@ -5,7 +5,14 @@ from grave import grave
 
 
 graph = nx.barbell_graph(10, 14)
+nx.set_node_attributes(graph, dict(graph.degree()), 'degree')
+
+
+def degree_colorer(node_attributes):
+    if node_attributes['degree'] > 5:
+        return {'color': 'r'}
+    return {'color': 'b'}
+
 
 fig, ax = plt.subplots()
-
-grave.plot_network(graph, ax=ax, node_style={'color': 'r'})
+grave.plot_network(graph, ax=ax, node_style=degree_colorer)
