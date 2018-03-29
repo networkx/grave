170c2859b22b2f136d334db1f9055d99cdb60a42
Thomas A Caswell <tcaswell@gmail.com>
2018-03-29 14:19:11 -0700

    FIX: formatting / typo in style.py

--- grave/style.py
+++ grave/style.py
@@ -9,7 +9,7 @@ def apply_style(style, item_iterable):
         for item, item_attr in item_iterable:
             styles[item] = style
     else:
-        raise TypeError("style must be dict or callable,"\
+        raise TypeError("style must be dict or callable,"
                         " got {0}".format(type(style)))
     return styles
 
@@ -21,4 +21,4 @@ def generate_node_styles(network, node_style):
 
 def generate_edge_styles(network, edge_style):
     # dict of edge tuple -> edge_style_dict
-    return apply_style(edge_style, nework.edges.data())
+    return apply_style(edge_style, network.edges.data())
