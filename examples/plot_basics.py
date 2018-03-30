f79b039aa5c0b09b6dd29ec29b674850e2430b2d
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2018-03-29 21:58:19 -0700

    DOC minor clean ups in the examples

--- examples/plot_basics.py
+++ examples/plot_basics.py
@@ -1,12 +1,11 @@
 """
-GraVE Documentation
--------------------
+A simple network
+----------------
 
 Test
 """
 import networkx as nx
 import matplotlib.pyplot as plt
-import random
 from grave import plot_network
 
 
@@ -15,19 +14,6 @@ graph = nx.barbell_graph(10, 14)
 nx.set_node_attributes(graph, dict(graph.degree()), 'degree')
 
 
-def degree_colorer(node_attributes):
-    deg = node_attributes['degree']
-    shape = random.choice(['s', 'o', '^', 'v', '8'])
-    if deg > 5:
-        return {'color': 'r', 'size': 20*deg, 'shape': shape}
-    return {'color': 'b', 'size': 20*deg, 'shape': shape}
-
-
-def pathological_edge_style(edge_attrs):
-    return {'color': random.choice(['r', (0, 1, 0, .5), 'xkcd:ocean'])}
-
-
 fig, ax = plt.subplots()
-plot_network(graph, ax=ax, node_style=degree_colorer,
-             edge_style=pathological_edge_style)
+plot_network(graph, ax=ax)
 plt.show()
