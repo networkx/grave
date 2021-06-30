d130baf312ab4b587888bfca6531fa3d84c5918f
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2021-06-30 13:34:03 +0200

    DOC adding extra layouts to example

--- examples/plot_layout.py
+++ examples/plot_layout.py
@@ -13,7 +13,7 @@ from grave import grave
 graph = nx.barbell_graph(10, 14)
 nx.set_node_attributes(graph, dict(graph.degree()), 'degree')
 
-fig, axes = plt.subplots(nrows=2, ncols=2)
+fig, axes = plt.subplots(nrows=3, ncols=2, figsize=(4, 6))
 
 grave.plot_network(graph, ax=axes[0, 0], layout="spring")
 axes[0, 0].set_title("spring", fontweight="bold")
@@ -26,5 +26,12 @@ axes[0, 1].set_title("random", fontweight="bold")
 
 grave.plot_network(graph, ax=axes[1, 1], layout="spectral")
 axes[1, 1].set_title("spectral", fontweight="bold")
+
+grave.plot_network(graph, ax=axes[2, 0], layout="shell")
+axes[2, 0].set_title("shell", fontweight="bold")
+
+
+grave.plot_network(graph, ax=axes[2, 1], layout="kamada_kawai")
+axes[2, 1].set_title("kamada_kawai", fontweight="bold")
 
 plt.show()
